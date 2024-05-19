bb54918f6f3f46d3dbcec38d9950d565b30b7d21
Sean Hamm <seanhamm28@gmail.com>
2024-05-18 20:35:43 -0700

    fixed bug in handleFull constant

--- Mod6App/src/components/PersonalInfo.jsx
+++ Mod6App/src/components/PersonalInfo.jsx
@@ -1,19 +1,38 @@
-function PersonalInfo() {
+import {useState} from 'react'
+
+function PersonalInfo() {  
+
+    const [PersonalInfo, setPersonalInfo] = useState({
+      fullName: "",
+      email: "", 
+      phone: ""
+
+    });
+
+    const handleFullNameInputChange = (e) => {
+        setPersonalInfo(prevInfo => (
+            {...prevInfo, fullName: e.target.value}
+
+        ))
+    }
+
   return (
     <div>
       <h2>Personal Information</h2>
       <div>
         <label htmlfor="fullName">Full Name:</label>
-        <input type="text" id="fullName" name="fullName" value="" />
+        <input type="text" id="fullName" name="fullName" value={PersonalInfo.fullName} onChange={handleFullNameInputChange} />
       </div>
       <div>
         <label htmlfor="Email">Email:</label>
-        <input type="text" id="Email" name="Email" value="" />
+        <input type="text" id="Email" name="Email" value={PersonalInfo.email} />
       </div>
       <div>
         <label htmlfor="phone">Phone Number:</label>
-        <input type="text" id="phone" name="phone" value="" />
+        <input type="text" id="phone" name="phone" value={PersonalInfo.phone} />
       </div>
+
+      <h1>{PersonalInfo.fullName}</h1>
     </div>
   );
 }
