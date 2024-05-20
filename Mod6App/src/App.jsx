f3bf65ed8ba9e28f8637701785da5ec9fc635e8b
Sean Hamm <seanhamm28@gmail.com>
2024-05-19 21:51:51 -0700

    working personal info and Education

--- Mod6App/src/App.jsx
+++ Mod6App/src/App.jsx
@@ -39,18 +39,26 @@ function App() {
   };
 
   return (
-    <div id="container">
-      <div>
+    <div id="container" className="app-container">
+      <div className="form-container">
         <PersonalInfo onChange={handlePersonalInfoUpdate} />
+        {!display.displayEdu && (
+          <EducationForm
+            eduInfo={eduInfo}
+            onChange={handleEduInfoUpdate}
+            onSubmit={(e) => handleSubmit(e, "displayEdu")}
+          />
+        )}
       </div>
-      <div>
-        <h1>{personalInfo.fullName}</h1>
-        <h2>{personalInfo.email}</h2>
-        <h2>{personalInfo.phone}</h2>
-      </div>
-      <div>
-        {display.displayEdu ? (
-          <div>
+      <div className="display-container">
+        <div className="personal-info-display">
+          <h2>Personal Information</h2>
+          <h1>{personalInfo.fullName}</h1>
+          <h2>{personalInfo.email}</h2>
+          <h2>{personalInfo.phone}</h2>
+        </div>
+        {display.displayEdu && (
+          <div className="education-info-display">
             <h2>Education Information</h2>
             <p>School: {eduInfo.schoolName}</p>
             <p>Study Title: {eduInfo.studyTitle}</p>
@@ -58,12 +66,6 @@ function App() {
             <p>End Date: {eduInfo.endDate.toISOString().slice(0, 10)}</p>
             <button onClick={() => setDisplay({ displayEdu: false })}>Edit</button>
           </div>
-        ) : (
-          <EducationForm
-            eduInfo={eduInfo}
-            onChange={handleEduInfoUpdate}
-            onSubmit={(e) => handleSubmit(e, "displayEdu")}
-          />
         )}
       </div>
     </div>
--- Mod6App/src/components/EducationForm.jsx
+++ Mod6App/src/components/EducationForm.jsx
@@ -25,49 +25,59 @@ function EducationForm(props) {
     <>
       <h2 className="form-title">Education</h2>
       <form onSubmit={handleSubmit} className="form">
-        <label htmlFor="schoolName" className="form-label">
-          School Name:
-          <input
-            type="text"
-            placeholder="School Name"
-            value={eduInfo.schoolName}
-            name="schoolName"
-            onChange={handleInputChange}
-            className="form-input"
-          />
-        </label>
-        <label htmlFor="studyTitle" className="form-label">
-          Study Title:
-          <input
-            type="text"
-            placeholder="Study Title"
-            value={eduInfo.studyTitle}
-            name="studyTitle"
-            onChange={handleInputChange}
-            className="form-input"
-          />
-        </label>
-        <label htmlFor="startDate" className="form-label">
-          Start Date:
-          <input
-            type="date"
-            value={eduInfo.startDate.toISOString().slice(0, 10)}
-            name="startDate"
-            onChange={handleInputChange}
-            className="form-input form-date-input"
-          />
-        </label>
-        <label htmlFor="endDate" className="form-label">
-          End Date:
-          <input
-            type="date"
-            value={eduInfo.endDate.toISOString().slice(0, 10)}
-            name="endDate"
-            onChange={handleInputChange}
-            className="form-input form-date-input"
-          />
-        </label>
-        <button className="form-btn" type="submit">Submit</button>
+        <div className="form-group">
+          <label htmlFor="schoolName" className="form-label">
+            School Name:
+            <input
+              type="text"
+              placeholder="School Name"
+              value={eduInfo.schoolName}
+              name="schoolName"
+              onChange={handleInputChange}
+              className="form-input"
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label htmlFor="studyTitle" className="form-label">
+            Study Title:
+            <input
+              type="text"
+              placeholder="Study Title"
+              value={eduInfo.studyTitle}
+              name="studyTitle"
+              onChange={handleInputChange}
+              className="form-input"
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label htmlFor="startDate" className="form-label">
+            Start Date:
+            <input
+              type="date"
+              value={eduInfo.startDate.toISOString().slice(0, 10)}
+              name="startDate"
+              onChange={handleInputChange}
+              className="form-input form-date-input"
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label htmlFor="endDate" className="form-label">
+            End Date:
+            <input
+              type="date"
+              value={eduInfo.endDate.toISOString().slice(0, 10)}
+              name="endDate"
+              onChange={handleInputChange}
+              className="form-input form-date-input"
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <button className="form-btn" type="submit">Submit</button>
+        </div>
       </form>
     </>
   );
